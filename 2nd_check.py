156c13ff6d57cdb8c0a1dc813f3eccfd5b9a4a53
siddhant-tyagi-WG <125998479+siddhant-tyagi-WG@users.noreply.github.com>
2023-02-23 11:16:01 +0530

    commit

--- 2nd_check.py
+++ 2nd_check.py
@@ -1,3 +1,11 @@
 x=34.908765454
 x=round(x,2)
 print(x)
+print('----------------')
+k=2
+k=k+1
+k=str(k)
+print(k)
+k=int(k)
+print(k)
+
--- web_scraping_project_using_class.py
+++ web_scraping_project_using_class.py
@@ -8,16 +8,27 @@ class google_websites:
     def func(self):
         page = requests.get(self.val)
         soup = BeautifulSoup(page.content, 'html.parser')
-        num = 50
+        num = 100
         links = soup.findAll("a")
         cnt = 1
-        for link in soup.find_all("a", href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
-            print(cnt, end=" ")
-            print(re.split(":(?=http)", link["href"].replace("/url?q=", "")))
-            cnt = cnt + 1
-            num = num - 1
+        k = 1
+        while (num != 0):
+            for link in soup.find_all("a", href=re.compile("(?<=/url\?q=)(htt.*://.*)")):
+                print(cnt, end=" ")
+                print(re.split(":(?=http)", link["href"].replace("/url?q=", "")))
+                cnt = cnt + 1
+                num = num - 1
+                if (num == 0):
+                    break
             if (num == 0):
                 break
+            elif (num != 0):
+                k = int(k)
+                k = k + 1
+                k = str(k)
+                url = f'https://www.google.com/search?q={search_query}/page' + k
+                page = requests.get(url)
+                soup = BeautifulSoup(page.content, 'html.parser')
 search_query=input("enter what you want to google: ")
 url = f'https://www.google.com/search?q={search_query}'
 my_obj=google_websites(val=url)
