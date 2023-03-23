33c382472c410c0c51480414ca5094aa251d87c5
siddhant-tyagi-WG <125998479+siddhant-tyagi-WG@users.noreply.github.com>
2023-03-24 00:02:57 +0530

    done

--- check.py
+++ check.py
@@ -1,2 +1,17 @@
-x=3.4
-print(type(x))
+def hello(func):
+    def wrapper_hello(*args,**kwargs):
+        print("Hello!")
+        print(f'{kwargs}')
+        val = func(*args,**kwargs)
+
+        print("Bye!")
+        return val
+    return wrapper_hello
+
+@hello
+def inc(n):
+    return n+1
+
+if __name__ == "__main__":
+    n = inc(n=3)
+    print(n)
